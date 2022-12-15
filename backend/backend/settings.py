8ed433a70e2b02db666a9b6575a338a0c8bb31de
ChristyAlex2147215 <christy.alex@mca.christuniversity.in>
2022-12-15 07:20:16 +0530

    final

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,10 +81,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+
+     'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'ccdatabse',  
+        'USER': 'admin',  
+        'PASSWORD': 'hello123',  
+        'HOST': 'database-1.c1h8tplwfs1y.us-east-1.rds.amazonaws.com',  
+        'PORT': '3306', 
+    }  
 }
 
 
